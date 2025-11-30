aa4ca66a63823cdc19766579bf12751d7ebedf07
Hamse Mohamed <151635250+hamse122@users.noreply.github.com>
2025-12-01 02:00:03 +0300

    Refactor EventEmitter to use Map for events

--- utils/eventEmitter.js
+++ utils/eventEmitter.js
@@ -1,39 +1,77 @@
-// Simple Event Emitter implementation
 class EventEmitter {
     constructor() {
-        this.events = {};
+        this.events = new Map();
     }
-    
+
     on(event, listener) {
-        if (!this.events[event]) {
-            this.events[event] = [];
+        if (!this.events.has(event)) {
+            this.events.set(event, []);
         }
-        this.events[event].push(listener);
+        this.events.get(event).push(listener);
         return this;
     }
-    
-    emit(event, ...args) {
-        if (this.events[event]) {
-            this.events[event].forEach(listener => listener.apply(this, args));
+
+    prependListener(event, listener) {
+        if (!this.events.has(event)) {
+            this.events.set(event, []);
         }
+        this.events.get(event).unshift(listener);
         return this;
     }
-    
-    off(event, listenerToRemove) {
-        if (this.events[event]) {
-            this.events[event] = this.events[event].filter(listener => listener !== listenerToRemove);
-        }
-        return this;
-    }
-    
+
     once(event, listener) {
-        const onceWrapper = (...args) => {
-            listener.apply(this, args);
-            this.off(event, onceWrapper);
+        const wrapper = (...args) => {
+            listener(...args);
+            this.off(event, wrapper);
         };
-        return this.on(event, onceWrapper);
+        return this.on(event, wrapper);
+    }
+
+    prependOnceListener(event, listener) {
+        const wrapper = (...args) => {
+            listener(...args);
+            this.off(event, wrapper);
+        };
+        return this.prependListener(event, wrapper);
+    }
+
+    emit(event, ...args) {
+        const listeners = this.events.get(event);
+        if (!listeners) return false;
+
+        // Copy array to prevent mutation during iteration
+        [...listeners].forEach(listener => listener(...args));
+        return true;
+    }
+
+    off(event, listenerToRemove) {
+        if (!this.events.has(event)) return this;
+
+        const filtered = this.events
+            .get(event)
+            .filter(listener => listener !== listenerToRemove);
+
+        this.events.set(event, filtered);
+
+        return this;
+    }
+
+    removeAllListeners(event) {
+        if (event) {
+            this.events.delete(event);
+        } else {
+            this.events.clear();
+        }
+        return this;
+    }
+
+    listenerCount(event) {
+        return this.events.has(event) ? this.events.get(event).length : 0;
+    }
+
+    eventNames() {
+        return [...this.events.keys()];
     }
 }
 
 module.exports = EventEmitter;
-
