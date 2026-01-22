68556172e9acbfa987ed8fa2ed4b666501d1da04
Hamse Mohamed <151635250+hamse122@users.noreply.github.com>
2026-01-23 02:34:40 +0300

    Enhance Middleware class with new methods

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,38 +1,63 @@
-// Middleware pipeline system
+// Advanced middleware pipeline
 class Middleware {
-    constructor() {
-        this.middlewares = [];
+  constructor(middlewares = []) {
+    this.middlewares = [...middlewares];
+  }
+
+  use(fn) {
+    if (typeof fn !== 'function') {
+      throw new TypeError('Middleware must be a function');
     }
+    this.middlewares.push(fn);
+    return this;
+  }
 
-    use(middleware) {
-        this.middlewares.push(middleware);
-        return this;
-    }
+  remove(fn) {
+    this.middlewares = this.middlewares.filter(mw => mw !== fn);
+    return this;
+  }
 
-    async execute(context, finalHandler) {
-        let index = 0;
+  clear() {
+    this.middlewares.length = 0;
+    return this;
+  }
 
-        const next = async () => {
-            if (index < this.middlewares.length) {
-                const middleware = this.middlewares[index++];
-                await middleware(context, next);
-            } else if (finalHandler) {
-                await finalHandler(context);
-            }
-        };
+  clone() {
+    return new Middleware(this.middlewares);
+  }
 
-        await next();
+  compose() {
+    const middlewares = this.middlewares;
+
+    return async function execute(context = {}, finalHandler) {
+      let index = -1;
+
+      async function dispatch(i) {
+        if (i <= index) {
+          throw new Error('next() called multiple times');
+        }
+        index = i;
+
+        let fn = middlewares[i];
+        if (i === middlewares.length) fn = finalHandler;
+        if (!fn) return;
+
+        return await fn(context, () => dispatch(i + 1));
+      }
+
+      try {
+        await dispatch(0);
         return context;
-    }
+      } catch (err) {
+        context.error = err;
+        throw err;
+      }
+    };
+  }
 
-    compose() {
-        return (context, finalHandler) => this.execute(context, finalHandler);
-    }
-
-    clear() {
-        this.middlewares = [];
-    }
+  async execute(context = {}, finalHandler) {
+    return this.compose()(context, finalHandler);
+  }
 }
 
 module.exports = Middleware;
-
